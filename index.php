b33707b7a586d8480c2634fbb7b61464b5c5a522
Knudsen2 <73479499+Knudsen2@users.noreply.github.com>
2021-05-19 08:54:16 +0200

    Oprettet index, nav, css fil og messageboard

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aros & Søn</title>
+    <link rel="stylesheet" href="css/index.css">
+  </head>
+  <body>
+    <?php include('navigation.php') ?>
+  
+    <img src="https://via.placeholder.com/150" alt="Aros & søn logo" class="center">
+
+    <form method="post">
+      <br>
+      <input type="text" name="" placeholder="Indtast brugernavn" class="center">
+      <br>
+      <input type="password" name="" placeholder="Indtast adgangskode" class="center">
+      <br>
+      <button type="submit" class="center">Log-in</button>
+    </form>
+
+
+  </body>
+</html>
---
+++ navigation.php
@@ -0,0 +1,5 @@
+<ul>
+  <li> <a href="index.php"><img src="https://via.placeholder.com/50" alt="Aros & søn logo"></a> </li>
+  <li> <a href="customers.php">Kunder</a> </li>
+  <li> <a href="coworkers.php">Medarbejdere</a> </li>
+</ul>
