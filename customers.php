8becd7e3a193ff947f212e983ce4983eada2da9a
Knudsen2 <73479499+Knudsen2@users.noreply.github.com>
2021-05-19 13:47:03 +0200

    rename, centering and new file
    kundeoprettelse.php er blevet renamet til create-customers.php
    Yderligere står indput felterne på samme side nu i midten.
    Customers.php er også blevet oprettet

---
+++ customers.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Kunder</title>
+  </head>
+  <body>
+  <?php include('navigation.php') ?>
+    <a href="create-customers.php">Opret ny kunde</a>
+
+
+  </body>
+</html>
