f37b683379a7277e3285b96e81a3bfa3dd5eacc2
amda20 <amda27579@edu.ucl.dk>
2021-05-19 12:32:58 +0200

    oprettelse af kundeoprettelse filen

---
+++ kundeoprettelse.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>
+  <br><input type="text" name="" placeholder="virksomheds navn" class="center">
+  <input type="text" name="" placeholder="Telefon nummer">
+  <p>
+  <br><input type="text" name="" placeholder="Adresse">
+  <input type="text" name="" placeholder="Mail">
+  <p>
+  <br><input type="text" name="" placeholder="By">
+  <input type="text" name="" placeholder="Kunde type">
+  <p>
+  <br><input type="text" name="" placeholder="Postnummer">
+  <input type="text" name="" placeholder="Ansvarlig medarbejder">
+  <p>
+  <br>
+  <button type="submit">Opret kunde</button>
+  </body>
+</html>
