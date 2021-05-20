20ddb76de5b3c397095acb830cabfc6ac998f88a
amda20 <amda27579@edu.ucl.dk>
2021-05-20 09:07:15 +0200

    oprettelse af slå op filen

---
+++ sloop.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <input id="txt1" type="text" style="width: 50%; height: 150px;">
+    <pre>
+    <button type="submit">Slå op</button>
+  </pre>
+  </body>
+</html>
