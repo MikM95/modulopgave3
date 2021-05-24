d9364d4a1551fa1ae0c1660211f7195905f16fc5
amda20 <amda27579@edu.ucl.dk>
2021-05-24 18:45:25 +0200

    ny felt i kundeoprettelse

---
+++ kundeoprettelse.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/index.css">
+    <?php include('navigation.php') ?>
+    <?php include('messageboard.php') ?>
+  </head>
+  <body>
+    <p>
+    <input type="text" name="" placeholder="virksomheds navn" class="center">
+    &nbsp;
+    <input type="text" name="" placeholder="Telefon nummer" class="center">
+    <p>
+    <input type="text" name="" placeholder="Adresse" class="center">
+    &nbsp;
+    <input type="text" name="" placeholder="Mail" class="center">
+    <p>
+    &nbsp;
+    <input type="text" name="" placeholder="By" class="center">
+    <input type="text" name="" placeholder="Kunde type" class="center">
+    <p>
+    &nbsp;
+    <input type="text" name="" placeholder="Postnummer" class="center">
+    <input type="text" name="" placeholder="Ansvarlig medarbejder" class="center">
+    &nbsp;
+    <input type="text" name="" placeholder="Kontakt person" class="center">
+    <p>
+    &nbsp;
+    <button type="submit" class="center">Opret kunde</button>
+  </body>
+</html>
