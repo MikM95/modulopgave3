e381eeffb859c09e120b023d48eb7c8f050fd9a3
amda20 <amda27579@edu.ucl.dk>
2021-05-25 12:23:11 +0200

    slettet

--- sloop.php
+++ sloop.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="css/index.css">
+    <?php include('navigation.php') ?>
   </head>
   <body>
     <input id="txt1" type="text" style="width: 50%; height: 150px;">
