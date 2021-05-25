ebb1b3c078ca1221c5f5cddae7686b1770da0f8b
Knudsen2 <73479499+Knudsen2@users.noreply.github.com>
2021-05-25 16:27:50 +0200

    forsøg på at få flere firmanavn ud under den enkelte medarbejder

--- employees-single.php
+++ employees-single.php
@@ -13,7 +13,7 @@ $eid = $_GET['eid'];
   <body>
     <?php
     include('navigation.php');
-    $data = performQuery("SELECT f_name, l_name, mail, phone_emp, employee_role.role FROM employees INNER JOIN employee_role ON employees.role_id = employee_role.role_id WHERE id='$eid'");
+    $data = performQuery("SELECT f_name, l_name, employees.mail, phone_emp, employee_role.role, customers.comp_name FROM employees INNER JOIN employee_role ON employees.role_id = employee_role.role_id INNER JOIN customers ON employees.id = customers.assigned_employee WHERE employees.id=$eid");
     $info = mysqli_fetch_array($data);
     ?>
 
@@ -27,10 +27,10 @@ $eid = $_GET['eid'];
       <div class="right">
         <p class="center-text">Ansvarlig for flg. kunder:</p>
         <hr id="line">
-
+        <?php //Jeg kan kun få koden til smide et firma navn til skærmen, har forsøgt at tilføje et loop for at få den til at smide flere ud men det virker ikke, tror jeg skal snakke med kenneth ?>
+        <p><?php echo $info['comp_name']; ?> </p>
       </div>
     </div>
-
 
   </body>
 </html>
