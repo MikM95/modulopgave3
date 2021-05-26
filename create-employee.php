7c08ca73a6d58a9fc15f1822d7e1b013d2c3a06d
Knudsen2 <73479499+Knudsen2@users.noreply.github.com>
2021-05-26 23:52:49 +0200

    Select meny på opret medarbejder siden

--- create-employee.php
+++ create-employee.php
@@ -30,7 +30,6 @@ include('functions.php');
         </div>
         <div class="line-breaker">
           <input type="text" name="pay_num" placeholder="Lønnummer">
-          <input type="number" name="role_id" placeholder="Indtast rolle id">
         </div>
         <div class="line-breaker">
           <input type="email" name="mail" placeholder="Email">
@@ -40,11 +39,20 @@ include('functions.php');
           <input type="text" name="username" placeholder="Brugernavn">
           <input type="password" name="password" placeholder="Adgangskode">
         </div>
+        <div class="line-breaker">
+          <?php //på linje 42 til 51 har jeg trukket data ud af databasen, det er egentligt unødvendigt at lave det variblet på nuværende tidspunkt, da det ikke er muligt at oprette nye roller på siden, men hvis det engang bliver muligt opdaterer denne "select meny" automatisk opdateret med evt. nye roller ?>
+          <label for="role_id">Vælg medarbejder rolle: </label>
+          <select name="rolle_id">
+            <?php $data = performQuery("select * from employee_role");
+            while($row = mysqli_fetch_assoc($data)) { ?>
+              <option value="<?php echo $row['role_id']; ?>"><?php echo $row['role'];?></option>
+            <?php } ?>
+          </select>
+        </div>
         <div class="button">
         <button type="submit">Opret medarbejder</button>
       </form>
     </div>
-
 
     <?php
 
@@ -52,7 +60,7 @@ include('functions.php');
 
         if (isset($_POST['f_name'], $_POST['l_name'], $_POST['address'], $_POST['city'], $_POST['postal'], $_POST['pay_num'], $_POST['mail'], $_POST['phone_emp'], $_POST['username'], $_POST['password']))
         //der er noget med isset der har en max værdi af en art. Der kan kun være 10 $_POST[], vi har 11
-        //Tina har ret i hvad hun skriver, så jeg har sletet så der kun er 10 $_post værdier det betyder at den ikke tjekker efter en værdi i hiring date + rolle id men begge felter skal stadig udfyldes - hvis alle felter bliver udfyldt virker formularen nu! 
+        //Tina har ret i hvad hun skriver, så jeg har sletet så der kun er 10 $_post værdier det betyder at den ikke tjekker efter en værdi i hiring date + rolle id men begge felter skal stadig udfyldes - hvis alle felter bliver udfyldt virker formularen nu!
           {
             $f_name = $_POST['f_name'];
             $l_name = $_POST['l_name'];
