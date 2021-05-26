f865421cb3739068a8943287a1a9ee11762faeca
Knudsen2 <73479499+Knudsen2@users.noreply.github.com>
2021-05-26 15:12:51 +0200

    Det er nu muligt at se beskeder oprettet i phpmyadmin
    Jeg mangler at gøre det muligt at oprette et opslag op messageboardet

--- messageboard.php
+++ messageboard.php
@@ -1,15 +1,33 @@
+<?php include('functions.php') ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Forside</title>
+    <link rel="stylesheet" href="css/index.css">
   </head>
   <body>
-    <?php include('navigation.php') ?>
-
-    <div class="container">
-
+    <?php include('navigation.php'); ?>
+    <?php $db_data = performQuery("SELECT dato, message, employees.f_name, employees.l_name from messages INNER JOIN employees on messages.sent_by = employees.id ORDER BY dato desc");
+    while($row = mysqli_fetch_assoc($db_data)) { ?>
+      <div class="flex-container">
+        <div class="flex-child">
+            <p>Slået op af: <?php echo $row['f_name'] ." ". $row['l_name']; ?></p>
+            <p>Tidspunkt: <?php echo $row['dato']; ?></p>
+        </div>
+        <div class="flex-child">
+          <p><?php echo $row['message']; ?> </p>
+        </div>
     </div>
+
+
+
+
+
+    <?php } ?>
+
+
+
 
   </body>
 </html>
